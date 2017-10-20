786609fa763a5073d9c589d6491de2250645ebf2
Alexander Boden <systems@tripattern.com>
2017-10-20 14:48:24 +0200

    Create oraclejdk.sh

---
+++ os/linux/ubuntu/etc/profile.d/oraclejdk.sh
@@ -0,0 +1,13 @@
+#export J2SDKDIR=/usr/lib/jvm/oracle_jdk7
+#export J2REDIR=/usr/lib/jvm/oracle_jdk7/jre
+#export PATH=$PATH:/usr/lib/jvm/oracle_jdk7/bin:/usr/lib/jvm/oracle_jdk7/db/bin:/usr/lib/jvm/oracle_jdk7/jre/bin
+#export JAVA_HOME=/usr/lib/jvm/oracle_jdk7
+#export DERBY_HOME=/usr/lib/jvm/oracle_jdk7/db
+
+# Java 8
+export IDEA_JDK=/usr/lib/jvm/oracle_jdk8
+#export J2SDKDIR=/usr/lib/jvm/oracle_jdk8
+#export J2REDIR=/usr/lib/jvm/oracle_jdk8/jre
+export PATH=$PATH:/usr/lib/jvm/oracle_jdk8/bin:/usr/lib/jvm/oracle_jdk8/db/bin:/usr/lib/jvm/oracle_jdk8/jre/bin
+export JAVA_HOME=/usr/lib/jvm/oracle_jdk8
+#export DERBY_HOME=/usr/lib/jvm/oracle_jdk8/db
