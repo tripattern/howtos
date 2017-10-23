6b3910630523a871d0cf75d1b404f295a66e4cbc
tripattern <systems@tripattern.com>
2017-10-23 10:30:12 +0200

    pull script refactored

--- scripts/gitPullAll.sh
+++ scripts/gitPullAll.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+(
+  cd "${1}"
+  for i in $(ls -d */)
+  do
+    (
+      cd "$i"
+      git config user.name "tripattern"
+      git config user.email "systems@tripattern.com"
+      ls
+    )
+  done 
+)
